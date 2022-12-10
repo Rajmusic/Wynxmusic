23155b4d1b79356a64df8a44c0cdc7418f6c910a
MUSIC <116700603+Rajmusic@users.noreply.github.com>
2022-12-10 08:47:46 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
-GIT_TOKEN = getenv("GIT_TOKEN", None)
+GIT_TOKEN = getenv("GIT_TOKEN", "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")
 
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/StarbotUpdate")
